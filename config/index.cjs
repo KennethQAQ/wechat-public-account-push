22bab605a62d625e5e84bb68473c8c36aca0e0d1
Kenneth Zhong <74444801+KennethQAQ@users.noreply.github.com>
2023-07-12 01:26:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oP12u6VJrGJYQU8C7XmR-ucfMwhE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'IcfLJ4mn94NjbdhypZRc5n7FANaB5fUolIbuaY7VOwQ',
+      useTemplateId: 'JazNxvJ2lMzbUne6s0lxvzchzA-V4sxIpreJUQFCNoQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-17',
       festivals: [
@@ -32,12 +32,12 @@ const USER_CONFIG = {
           type: '*生日', name: '小懒鹅', year: '2001', date: '05-17',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '小懒鹅', year: '2001', date: '05-17',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2023', date: '03-02',
-        },
+        //{
+          //type: '生日', name: '小懒鹅', year: '2001', date: '05-17',
+        //},
+        //{
+          //type: '节日', name: '相识纪念日', year: '2023', date: '03-02',
+        //},
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
