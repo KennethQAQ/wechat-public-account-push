c3f97d602b33096d81af107b17b244e51dcbcc0e
Kenneth Zhong <74444801+KennethQAQ@users.noreply.github.com>
2023-07-12 08:39:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,7 +7,7 @@
  */
 const USER_CONFIG = {
   
-  USE_PASSAGE: 'push-plus',
+  USE_PASSAGE: 'server-chan',
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
 
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小懒鹅',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'bf4467fb165e4bfcb4795b1fd036970d',
+      id: 'SCT216056TVpG42TkzUm7lz9OrrK1NNRkZ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
