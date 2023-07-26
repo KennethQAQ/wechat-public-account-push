99599662074c0784e8c3d6fe7ee95a9cdcca1f07
Kenneth Zhong <74444801+KennethQAQ@users.noreply.github.com>
2023-07-26 16:11:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,6 +51,39 @@ const USER_CONFIG = {
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
+    {
+      // 想要发送的人的名字
+      name: '小懒鹅',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'SCT216056TVpG42TkzUm7lz9OrrK1NNRkZ',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '0001',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '05-17',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        //{
+          //type: '*生日', name: '小懒鹅', year: '2001', date: '05-17',
+        //},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '小懒鹅', year: '2001', date: '05-17',
+        },
+        {
+          type: '节日', name: '恋爱纪念日2', year: '2023', date: '07-16',
+        },
+        {
+          type: '节日', name: '恋爱纪念日1', year: '2019', date: '04-06',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-07-16' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
   ],
 
 
